cd5c0e2503e472c1ba6c023a61fd6d74fc863b02
Anisur Rahman <anisurlist3@gmail.com>
2018-07-31 18:34:29 +0600

    Layout clean up with blade syntax

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,101 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('main')
+@section('content')
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+  	<h1>Contact</h1>
+  	<form action="#" method="POST" role="form">
 
-    <title>Laravel | Contact us</title>
+  		<div class="form-group">
+  			<label for="email">Email:</label>
+  			<input type="text" name="email" class="form-control" id="email" placeholder="Input field">
+  		</div>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <a class="navbar-brand" href="/">Laravel Blog</a>
-      </div>
+			<div class="form-group">
+  			<label for="name">Name:</label>
+  			<input type="text" name="name" class="form-control" id="name" placeholder="Input field">
+  		</div>
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li><a href="/">Home <span class="sr-only">(current)</span></a></li>
-          <li><a href="/about">About</a></li>
-          <li class="active"><a href="/contact">Contact</a></li>
-          {{-- <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li> --}}
-        </ul>
-        {{-- <form class="navbar-form navbar-left">
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form> --}}
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#">Link</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Account <span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
+			<div class="form-group">
+  			<label for="message">Message:</label>
+  			<textarea name="message" id="message" class="form-control" rows="6" required="required"></textarea>
+  		</div>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-      	<h1>Contact</h1>
-      	<form action="#" method="POST" role="form">
-
-      		<div class="form-group">
-      			<label for="email">Email:</label>
-      			<input type="text" name="email" class="form-control" id="email" placeholder="Input field">
-      		</div>
-
-					<div class="form-group">
-      			<label for="name">Name:</label>
-      			<input type="text" name="name" class="form-control" id="name" placeholder="Input field">
-      		</div>
-
-					<div class="form-group">
-      			<label for="message">Message:</label>
-      			<textarea name="message" id="message" class="form-control" rows="6" required="required"></textarea>
-      		</div>
-
-      		<button type="button" class="btn btn-primary">Send</button>
-      	</form>
-      </div>
-    </div>
+  		<button type="button" class="btn btn-primary">Send</button>
+  	</form>
   </div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>  </body>
-</html>
+</div>
+@endsection
